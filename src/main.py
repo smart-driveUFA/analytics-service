a5ecfc04e701f06d55764cc9df6fa305a9230076
Konstantin <hardfiveeven@mail.ru>
2023-10-19 11:33:41 +0300

    added yandex api save to mongodb

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, APIRouter
+from dotenv import load_dotenv
+from fastapi import APIRouter, FastAPI
+
+from src.yandex_api.weather_api import router as yandex_router
+
+load_dotenv()
 
 app = FastAPI(
     docs_url="/",
 )
 
 main_router = APIRouter(prefix="/analytic/v1")
+main_router.include_router(yandex_router)
+app.include_router(main_router)
